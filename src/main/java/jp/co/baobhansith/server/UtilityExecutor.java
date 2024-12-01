ac3d819b7af2ffbd5c4e7908264c19075b4bc5f2
ytakasugi <sh7.tibi0129@gmail.com>
2024-12-01 21:05:17 +0900

    change files

--- src/main/java/jp/co/baobhansith/server/UtilityExecutor.java
+++ src/main/java/jp/co/baobhansith/server/UtilityExecutor.java
@@ -1,33 +1,13 @@
 package jp.co.baobhansith.server;
 
-import java.util.List;
-
 import jp.co.baobhansith.server.util.BaobhansithUtility;
 
 public class UtilityExecutor {
     private static final String CONFIG_PATH = "/home/ytakasugi/java-workspace/baobhansith/config.csv";
     private static final String TARGET_ID = "X00_00_000_4";
-    private static final int[] INDEX_ARRAY = {5, 6, 7};
-    private static final String[] TERAMS1 = {"HTTP", "ASYNC"};
-    private static final String[] TERAMS2 = {"HTTP", "HTTPS"};
 
     public static void main(String[] args) {
         try {
-            // String[] record = BaobhansithUtility.getRowByKey(CONFIG_PATH, TARGET_ID);
-            // String[] values = BaobhansithUtility.getDirectoryArray(record, INDEX_ARRAY);
-
-            // for (String value : values) {
-            //     System.out.println(value);
-            // }
-            
-            // String value = BaobhansithUtility.getFirstNonEmptyElement(record, 1);
-            // System.out.println(value);
-
-            // List<String[]> records = BaobhansithUtility.getRowsByKey(CONFIG_PATH, "X00_00_000_1", 0);
-
-            // for (String[] record : records) {
-            //     System.out.println(record[0]);
-            // }
 
             String[] record = BaobhansithUtility.getRowByKey(CONFIG_PATH, TARGET_ID);
             record = BaobhansithUtility.getNonEmptyElement(record, 3);
